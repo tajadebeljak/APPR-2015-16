fbc046f305428bf9ea03a448ad7d90edd523762d
Taja Debeljak <taja.debeljak95@gmail.com>
2016-01-16 22:41:49 +0100

    zem

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -6,17 +6,25 @@ library(dplyr)
 
 # Uvozimo zemljevid.
 zemljevid <- uvozi.zemljevid("http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/50m/cultural/ne_50m_admin_0_countries.zip",
-                             "ne_50m_admin_0_countries", encoding = "UTF-8")
+                             "ne_50m_admin_0_countries", encoding = "Windows-1252")
 
 
 
 
 # Preuredimo podatke, da jih bomo lahko izrisali na zemljevid.
 rownames(tabela4)<-tabela4$Drzava
-destinacije2 <- preuredi(tabela4, zemljevid, sovereignt)
+m <- match(zemljevid$name, tabela4$Drzava)
+zemljevid$Prihodi2014 <- tabela4$Prihodi2014[m]
 
+
+pretvori.zemljevid <- function(zemljevid) {
+  fo <- fortify(zemljevid)
+  data <- zemljevid@data
+  data$id <- as.character(0:(nrow(data)-1))
+  return(inner_join(fo, data, by="id"))
+}
 
 # Izračunamo povprečno velikost družine.
-druzine$povprecje <- apply(druzine[1:4], 1, function(x) sum(x*(1:4))/sum(x))
-min.povprecje <- min(druzine$povprecje, na.rm=TRUE)
-max.povprecje <- max(druzine$povprecje, na.rm=TRUE)
+#druzine$povprecje <- apply(druzine[1:4], 1, function(x) sum(x*(1:4))/sum(x))
+#min.povprecje <- min(druzine$povprecje, na.rm=TRUE)
+#max.povprecje <- max(druzine$povprecje, na.rm=TRUE)
