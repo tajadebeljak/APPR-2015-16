df2132d04e4640137f3f3e57fe9decd758094fd7
Taja Debeljak <taja.debeljak95@gmail.com>
2016-02-28 23:25:30 +0100

    analiza

--- analiza/analiza.r
+++ analiza/analiza.r
@@ -1,4 +1,10 @@
 # 4. faza: Analiza podatkov
 
+potovanja2 <- potovanja %>% filter (Povprecno.stevilo.nocitev.in.izdatkov=="Povprečni izdatki na turista na prenočitev (EUR)")
+
+graf_potovanj <- ggplot(potovanja2, aes(y=Meritve, x= Četrtletje)) + geom_point()
+
+graf_potovanj <- graf_potovanj + geom_smooth(method = "lm", formula =y ~ x)
+
 barve <- rainbow(length(levels(obcine[[7]])))
 names(barve) <- levels(obcine[[7]])
