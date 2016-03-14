31517c2fdc0e2252165d6d390f534a64c635036f
Taja Debeljak <taja.debeljak95@gmail.com>
2016-03-14 12:39:38 +0100

    napoved

--- analiza/analiza.r
+++ analiza/analiza.r
@@ -1,6 +1,8 @@
 # 4. faza: Analiza podatkov
 
+
 potovanja2 <- potovanja %>% filter (Povprecno.stevilo.nocitev.in.izdatkov=="Povprečni izdatki na turista na prenočitev (EUR)")
+potovanja2$Četrtletje <- gsub("Q", "0", potovanja2$Četrtletje) %>% as.numeric()
 
 graf_potovanj <- ggplot(potovanja2, aes(y=Meritve, x= Četrtletje)) + geom_point()
 
