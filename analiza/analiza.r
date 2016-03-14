00b5a33f81f5fe36baa5792f86e5e967fa14987c
Taja Debeljak <taja.debeljak95@gmail.com>
2016-03-14 12:54:09 +0100

    četrtletja

--- analiza/analiza.r
+++ analiza/analiza.r
@@ -2,11 +2,14 @@
 
 
 potovanja2 <- potovanja %>% filter (Povprecno.stevilo.nocitev.in.izdatkov=="Povprečni izdatki na turista na prenočitev (EUR)")
-potovanja2$Četrtletje <- gsub("Q", "0", potovanja2$Četrtletje) %>% as.numeric()
+potovanja2$Četrtletje <- gsub("Q1", ".00", potovanja2$Četrtletje) %>% as.numeric()
+potovanja2$Četrtletje <- gsub("Q2", ".25", potovanja2$Četrtletje) %>% as.numeric()
+potovanja2$Četrtletje <- gsub("Q3", ".50", potovanja2$Četrtletje) %>% as.numeric()
+potovanja2$Četrtletje <- gsub("Q4", ".75", potovanja2$Četrtletje) %>% as.numeric()
 
 graf_potovanj <- ggplot(potovanja2, aes(y=Meritve, x= Četrtletje)) + geom_point()
 
-graf_potovanj <- graf_potovanj + geom_smooth(method = "lm", formula =y ~ x)
+graf_potovanj1 <- graf_potovanj + geom_smooth(method = "lm", formula =y ~ x)
 
 #barve <- rainbow(length(levels(obcine[[7]])))
 #names(barve) <- levels(obcine[[7]])
