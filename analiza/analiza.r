003b41dea641c0ce7cb6dea04fff8f6ed047c322
Taja Debeljak <taja.debeljak95@gmail.com>
2016-03-31 22:10:24 +0200

    dokončano

--- analiza/analiza.r
+++ analiza/analiza.r
@@ -16,7 +16,7 @@ graf_potovanj1 <- graf_potovanj + geom_smooth(method = "lm", formula =y ~ x)
 
 potovanja2_slovenija <- potovanja2 %>% filter (Destinacija=="Slovenija")
 graf_potovanj_slo <- ggplot(potovanja2_slovenija, aes(y=Meritve, x= datum)) + geom_point()
-graf_potovanj2 <- graf_potovanj_slo + geom_smooth(method = "lm", formula =y ~ x)
+graf_potovanj2 <- graf_potovanj_slo + geom_smooth(method = "lm", formula =y ~ x) + labs(list(title="Povprečni izdatki (Slovenija)", x="Datum", y="Meritve"))
 
 potovanja2_tujina <- potovanja2 %>% filter (Destinacija=="Tujina")
 graf_potovanj_tuj <- ggplot(potovanja2_tujina, aes(y=Meritve, x= datum)) + geom_point()
@@ -31,7 +31,7 @@ graf_potovanj3 <- graf_potovanj_tuj + geom_smooth(method = "lm", formula =y ~ x)
 
 
 graf_prenočitve_slo <- ggplot(št_prenočitev_slo, aes(y=Meritve, x= datum)) + geom_point()
-graf_prenočitve1 <- graf_prenočitve_slo + geom_smooth(method = "lm", formula =y ~ x)
+graf_prenočitve1 <- graf_prenočitve_slo + geom_smooth(method = "lm", formula =y ~ x) + labs(list(title="Število prenočitev (Slovenija)", x="Datum", y="Meritve"))
 
 št_prenočitev_tuj <- potovanja %>% filter (Povprecno.stevilo.nocitev.in.izdatkov=="Povprečno število prenočitev", Destinacija=="Tujina")
 št_prenočitev_tuj <- št_prenočitev_tuj %>%
